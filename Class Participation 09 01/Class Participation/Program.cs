14e67620fd4c5eec01489adf0076f9033f5b8202
Joseph Wenrich <joseph@ou.edu>
2020-09-01 18:02:42 -0500

    Completed examples for day.

--- Class Participation 09 01/Class Participation/Program.cs
+++ Class Participation 09 01/Class Participation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Class_Participation
 {
@@ -6,7 +7,34 @@ namespace Class_Participation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+            Student stud1 = new Student();
+            Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            stud1.name="Micah";
+            stud1.id = 2;
+            students.Add(stud1);
+
+            Student stud2 = new Student("Adam", 1);
+            students.Add(stud2);
+
+            foreach (var stud in students)
+            {
+                Console.WriteLine(stud);
+            }
+
+            DateTime current = DateTime.Now;
+            DateTime past = Convert.ToDateTime("1/1/20");
+
+            var result = current - past;
+
+
+
+            TimeSpan difference = current-stud1.enrollDate;
+            Console.WriteLine($"It has been {difference.TotalMilliseconds} since {stud1.name} has enrolled");
+
+            Console.WriteLine($"There are currently {Student.totalStudents} enrolled");
+            //Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            //Console.WriteLine($"{stud2.name} has an id of {stud2.id}");
         }
     }
 }
