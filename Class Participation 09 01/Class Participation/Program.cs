aebcee00af9f918e6c7f194cda70f360883bb7f2
Joseph Wenrich <joseph@ou.edu>
2020-09-01 17:18:53 -0500

    Created solution to practice classes and files.

---
+++ Class Participation 09 01/Class Participation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
